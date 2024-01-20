e69efb0ad1461fb6df9ea254675d80b23c1e610a
peterfeld <89661123000@mail.ru>
2024-01-20 20:52:31 +0300

    Обновляет слайдер в топе, добавляет слайдер в поп

---
+++ initializationSlick.js
@@ -0,0 +1,16 @@
+$(".multiple-items").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 3
+  });  
+
+  $(".single-item").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  
